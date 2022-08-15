710f5a14b35f2be75a092b277442760c8bb5cf1a
Renan <renandacostanunes@gmail.com>
2022-08-15 20:22:47 -0300

    Exercícios propostos

---
+++ exercicios/BeeCrowd.py
@@ -0,0 +1,15 @@
+#Ecercício número 1
+
+print ("Hello World!!!")
+
+#Exercício número 2
+
+print ("Escreva o primeiro número")
+X = int(input())
+print ("Escreva o segundo número")
+Y = int(input())
+
+
+Z = X+Y
+
+print ("O resultado da soma de",X,"+",Y,"é igual a",Z)
